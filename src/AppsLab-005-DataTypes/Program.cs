7e0cf7a946cf3322ed60b617bcae1860fff50449
Matúš Šimora <matussimora10@outlook.com>
2025-09-24 10:39:04 +0200

    hello

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿const string Welcome = "Vitajte v AppsLabe!";
+
 int mojeCislo = 1234;
 string mojeSlovo = "Ahoj, svet";
 bool mojaBoolHodnota = true;
@@ -6,6 +7,8 @@ DateTime dnesnyDatum = DateTime.Now;
 float myFloat = 5.75f;
 int sucet = mojeCislo + 5;
 var myVar = "Toto je var string";
+const int MaxStudents = 15;
+
 
 Console.WriteLine(mojeCislo.ToString("000000"));
 Console.WriteLine(mojeSlovo);
@@ -14,7 +17,8 @@ Console.WriteLine($"dnes je robotkac{dnesnyDatum.ToString("yyyy-MM-dd")}");
 Console.WriteLine($"Bool Hodnota je: {mojaBoolHodnota}");
 Console.WriteLine(myVar);
 Console.WriteLine(myFloat);
-
+Console.WriteLine("Maximálny počet študentov v triede je: " + MaxStudents);
+Console.WriteLine(Welcome);
 
 
 
