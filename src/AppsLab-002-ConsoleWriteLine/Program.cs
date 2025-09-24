a03dfebe141dbfc7c3db520c15871cfc850cd43b
Matúš Šimora <matussimora10@outlook.com>
2025-09-24 09:32:02 +0200

    posielam

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,4 +1,11 @@
-﻿int age = 15;
-string name = "Conoor";
+﻿int age = 8;
+string name = "Makar";
+
 Console.WriteLine(age);
 Console.WriteLine(name);
+
+Console.WriteLine($"My name is {name} and Im {age} years old.");
+
+
+
+
--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int mojeCislo = 1234;
+string mojeSlovo = "Ahoj, svet";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+float myFloat = 5.75f;
+int sucet = mojeCislo + 5;
+var myVar = "Toto je var string";
+
+Console.WriteLine(mojeCislo.ToString("000000"));
+Console.WriteLine(mojeSlovo);
+Console.WriteLine($"Súčet je: {sucet}");
+Console.WriteLine($"dnes je robotkac{dnesnyDatum.ToString("yyyy-MM-dd")}");
+Console.WriteLine($"Bool Hodnota je: {mojaBoolHodnota}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
