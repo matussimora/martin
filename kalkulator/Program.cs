4b6ddad6a215d02a839085f1cbefabcac16fc697
Matúš Šimora <matussimora10@outlook.com>
2025-10-01 13:16:16 +0200

    du

---
+++ kalkulator/Program.cs
@@ -0,0 +1,78 @@
+﻿//using System.Numerics;
+//{
+//    int a = 20;
+//    int b = 5;
+
+//    int sum = a + b;
+//    int differenc = a - b;
+//    int product = a * b;
+//    int division = a / b;
+//    int remainder = a % b;
+
+//    bool rovnost = a == b;
+//    bool nerovnost = a != b;
+//    bool väčšíako = a > b;
+//    bool menšíako = a < b;
+//    bool väčšíaleborovnýako = a >= b;
+//    bool menšíaleborovnýako = a <= b;
+//}
+
+//using System.Numerics;
+
+//bool a = true;
+//bool b = 74;
+//bool c = 42;
+
+//bool andBool1 = a && (b > c);
+//bool andBool2 = a && (b==c);
+//bool andBool3 = a && (b == c) || (c < b);
+//bool andBool4 = a && ((b ==c) || (c < b));
+//int d = 0;
+
+
+//načitam si cislo s klavesnice 
+
+//ak je to 6-vypis ides este raz 
+
+//int age = 20;
+
+//if (age <= 18) ;
+//{
+//    Console.WriteLine("Som dospely ");
+//    Console.WriteLine("Dam si pivo");
+//}
+
+//Console.WriteLine("Hoď kockou");
+//string s = Console.ReadLine();
+
+//int cislo = int.Parse(s);
+
+//if (cislo >= 1 && cislo <= 6);
+//{ 
+//    Console.WriteLine("Hurá hodil si kockou");
+//}
+
+Console.WriteLine("Zadaj prvé cislo");
+int cislo1 = int .Parse(Console.ReadLine());
+Console.WriteLine("WOW super");
+Console.WriteLine("Zadaj druhe cislo");
+int cislo2 = int .Parse(Console.ReadLine());
+
+int sucet = cislo1 + cislo2;
+Console.WriteLine($"Sucet {sucet}");
+int rozdiel = cislo1 - cislo2;
+Console.WriteLine($"Rozdiel{rozdiel}");
+int sucin = cislo1 * cislo2;
+Console.WriteLine($"sucin{sucin}");
+int podiel = cislo1 / cislo2;
+
+
+
+
+
+
+
+
+
+
+
--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,17 +1,14 @@
 ﻿{
-    Console.WriteLine("Ahoj ako sa voláš?");
-    string? meno = Console.ReadLine();
-    Console.WriteLine(" Ahoj, " + meno + " koľko máš rokov. ");
-    string vstup = Console.ReadLine() ?? "0";
-    int vek = int.Parse(vstup);
-    Console.WriteLine("Aha, máš teda " + vek + " rokov.");
+    //privitanie
+    Console.WriteLine("Moja prvá kalkulačka");
 
-    Console.WriteLine("Aké je tvoje obľúbené jedlo?");
-    string? jedlo = Console.ReadLine() ?? "0";
-    Console.WriteLine("Aha takže tvoje obľúbene jedlo je" +  jedlo);
+    //načitam čislo
+    string? prveCislo = Console.ReadLine();
+    //načitam 2. číslo
+    string? druheCislo = Console.ReadLine();
+    //spravim sucet
+    int sucet = int.Parse(prveCislo) + int.Parse(druheCislo);
+    //vypíšem sučet
+    Console.WriteLine($"Súčet:" + sucet);
 
-    Console.WriteLine("Stlač klávesu");
-    var key = Console.ReadKey();
-    Console.WriteLine();      
-    Console.WriteLine($"Stlačili ste klávesu: {key.KeyChar}");
 }
