b5e24716d7ba68bad2dc8a710b16b06664aa9749
Matus <matussimora10@gmail.com>
2025-10-08 10:57:09 +0200

    ulohy

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,17 +1,58 @@
-﻿Console.WriteLine("Zadaj prvé číslo");
-int cislo1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Zadaj druhé číslo");
-int cislo2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Zadaj operáciu +,-,*,/,%");
-string operacia = Console.ReadLine();
+﻿//Console.WriteLine("Zadaj prvé číslo");
+//int cislo1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Zadaj druhé číslo");
+//int cislo2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Zadaj operáciu +,-,*,/,%");
+//string operacia = Console.ReadLine();
 
-int vysledok = operacia switch
+//int vysledok = operacia switch
+//{
+//    "+" => cislo1 + cislo2,
+//    "-" => cislo1 - cislo2,
+//    "*" => cislo1 * cislo2,
+//    "/" => cislo1 / cislo2,
+//    "%" => cislo1 % cislo2,
+//};
+
+//Console.WriteLine($"Tvoj vysledok je {vysledok}");
+
+//Console.WriteLine("Zadaj číslom deň týždňa");
+//int dayofweak = int .Parse(Console.ReadLine());
+
+//switch (dayofweak)
+//{
+//    case 1:
+//        Console.WriteLine("Pondelok");
+//        break;
+//        case 2:
+//        Console.WriteLine("Utorok");
+//        break;
+//        case 3:
+//        Console.WriteLine("Streda");
+//        break;
+//        case 4:
+//        Console.WriteLine("Štvrtok");
+//        break;
+//        case 5:
+//        Console.WriteLine("Piatok");
+//        break;
+//        case 6:
+//        Console.WriteLine("Sobota");
+//        break;
+//        case 7:
+//        Console.WriteLine("Nedeľa");
+//        break;
+//        default:
+//        Console.WriteLine("Neplatný deň");
+//        break;
+//}
+int numberDescription;
+int; numberDescription  = numberDescription switch
 {
-    "+" => cislo1 + cislo2,
-    "-" => cislo1 - cislo2,
-    "*" => cislo1 * cislo2,
-    "/" => cislo1 / cislo2,
-    "%" => cislo1 % cislo2,
+ 1 => "jeden",
+ 2 => "dva",
+ 3 => "tri",
+ _ => "Neznáme číslo"
 };
 
-Console.WriteLine($"Tvoj vysledok je {vysledok}");
+Console.WriteLine(numberDescription); //vypise:tri 
